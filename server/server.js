d6987010156cbbf2fe5e410a2a4cc1e3f14dfa9f
Simon Conway <simon.conway@capgemini.com>
2017-04-18 10:13:50 +0100

    Set up an Exress app

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-var express = require ('express.js')
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+console.log(publicPath);
+
+const express = require ('express')
+const port = process.env.PORT || 5000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(5000, () => {
+  console.log(`Server is up on port ${port}`)
+})
