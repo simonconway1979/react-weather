b0627d696db333288dc54b3382f3e2d0daab892b
Simon Conway <simon.conway@capgemini.com>
2017-04-18 14:57:33 +0100

    Set up boilerplate app

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      // Add path aliases here. This makes requiring files simpler. E.g.
+      // ComponentName: 'public/components/ComponentName.jsx',
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
