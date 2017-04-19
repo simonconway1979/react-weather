dba69b7793ade959d061fce2540d5a7427d952d9
Simon Conway <simon.conway@capgemini.com>
2017-04-19 16:41:01 +0100

    Added foundation and jquery

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=28851e81c8936ba78e3d022092c19f92&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=28851e81c8936ba78e3d022092c19f92&units=metric';
 
 module.exports = {
   getTemp: function (location) {
