6e8debdf3b93d4710b9ce22ec507174c69df61bd
Simon Conway <simon.conway@capgemini.com>
2017-04-19 15:10:58 +0100

    Updated for Heroku

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 
 const express = require ('express')
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 var app = express();
+
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      response.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`)
 })
