e587c8c3e64ca4edc5328feea8cca096fbde4656
Simon Conway <simon.conway@capgemini.com>
2017-04-20 17:13:41 +0100

    Added some information on the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = function(props) {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About the React Weather App</h1>
+      <p>The React Weather App uses Open Weather Map API to collect the weather information.
+        You can read more about Open Weather API<a href='https://openweathermap.org/api'> here</a></p>
+      <p>We are using Zurb Foundation styling. You can find info on Zurb
+      <a href="http://foundation.zurb.com/"> here</a></p>
+    </div>
   )
 };
 
