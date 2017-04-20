c5ebf9159a36b53446ee8993fb0999ac6f07af39
Simon Conway <simon.conway@capgemini.com>
2017-04-20 21:52:23 +0100

    Added some custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = function(props) {
   return (
     <div>
-      <h1 className="text-center">About the React Weather App</h1>
+      <h1 className="text-center page-title">About the React Weather App</h1>
       <p>The React Weather App uses Open Weather Map API to collect the weather information.
         You can read more about Open Weather API<a href='https://openweathermap.org/api'> here</a></p>
       <p>We are using Zurb Foundation styling. You can find info on Zurb
